c07beaa865fdc8666655e43c7d29df3a850600a8
Isaac Spencer <22ispencer@gmail.com>
2025-01-21 13:11:37 -0600

    refactored into classes

--- lua/calc/init.lua
+++ lua/calc/init.lua
@@ -1,88 +1,143 @@
 local M = {}
 
---- Evaluate a simple RPN calculation string
---- Evaluates the RPN expression from left to right
---- @param expr string The expression to evaluate
---- @return number? The calculated result
-function M.eval_expr(expr)
-	if type(expr) ~= "string" then
-		return
-	end
+---@class Calculator
+---@field stack number[] The stack of numbers to operate on
+---@field stack_buf number? The buffer contining the stack
+---@field stack_win number? The window contining the stack if one exists
+---@field tail string[] The history of operations
+---@field tail_buf number? The buffer contining the tail
+---@field tail_win number? The window contining the stack if one exists
+local Calculator = {
+	stack = {},
+	tail = {},
+}
+Calculator.__index = Calculator
 
-	local stack = { 0 }
-
-	-- Define chunks as containing: numbers, "+", "-", "*", "/"
-	for chunk in string.gmatch(expr, "([^%s]+)") do
-		local num = tonumber(chunk)
-		if num then
-			table.insert(stack, num)
-		elseif chunk == "+" then
-			local add_num = table.remove(stack)
-			stack[#stack] = stack[#stack] + add_num
-		elseif chunk == "-" then
-			local add_num = table.remove(stack)
-			stack[#stack] = stack[#stack] - add_num
-		elseif chunk == "*" then
-			local add_num = table.remove(stack)
-			stack[#stack] = stack[#stack] * add_num
-		elseif chunk == "/" then
-			local add_num = table.remove(stack)
-			stack[#stack] = stack[#stack] / add_num
-		end
-	end
-	return stack[#stack]
+function Calculator.create()
+	local calculator = setmetatable({}, Calculator)
+	return calculator
 end
 
----@param stack number[] The stack of numbers
----@param buf number The stack buffer number
--- ---@param win number The stack buffer number
-local function draw_stack(stack, buf)
-	local lines = {}
-	for _, val in ipairs(stack) do
-		table.insert(lines, tostring(val))
-	end
-	table.insert(lines, ".")
+--------------------------------------------------------------------------------
+--------------------------------------UI----------------------------------------
+--------------------------------------------------------------------------------
 
-	vim.api.nvim_set_option_value("modifiable", true, { buf = buf })
-	vim.api.nvim_buf_set_lines(buf, 0, -1, false, lines)
-	vim.api.nvim_set_option_value("modifiable", false, { buf = buf })
-	-- vim.api.nvim_win_set_cursor(win, { #stack + 1, 0 })
-end
+function Calculator:create_ui()
+	self.stack_buf = vim.api.nvim_create_buf(false, true)
 
----@param stack number[] The number stack
----@param buf number The stack buffer ID
----@param win number The stack window ID
-local function stack_keymaps(stack, buf, win)
-	vim.keymap.set("n", "q", function()
-		for k, _ in ipairs(stack) do
-			stack[k] = nil
-		end
-		vim.api.nvim_buf_delete(buf, {})
-		vim.api.nvim_win_close(win, true)
-	end, { buffer = buf })
-	vim.keymap.set("n", "5", function()
-		table.insert(stack, 5)
-		draw_stack(stack, buf)
-	end, { buffer = buf })
-end
+	vim.api.nvim_set_option_value("filetype", "calc", { buf = self.stack_buf })
 
----@type number[] The stack full of numbers
-M.stack = {}
-
-function M.open()
-	local stack_buf = vim.api.nvim_create_buf(false, true)
-
-	vim.api.nvim_set_option_value("filetype", "calc", { buf = stack_buf })
-
-	local win = vim.api.nvim_open_win(stack_buf, true, {
+	self.stack_win = vim.api.nvim_open_win(self.stack_buf, true, {
 		win = -1,
 		height = 10,
 		split = "below",
+		style = "minimal",
 	})
-	stack_keymaps(M.stack, stack_buf, win)
-	vim.api.nvim_set_option_value("relativenumber", false, { win = win })
+	self:init_stack_keymaps()
+	vim.api.nvim_set_option_value("relativenumber", false, { win = self.win })
 
-	draw_stack(M.stack, stack_buf)
+	self:draw_stack()
+end
+
+function Calculator:draw_stack()
+	if not self.stack_buf then
+		vim.notify("Must initialize ui before drawing", vim.log.levels.ERROR)
+		return
+	end
+	local lines = {}
+	for i, val in ipairs(self.stack) do
+		table.insert(lines, string.format("% 3d", #self.stack + 1 - i) .. ": " .. tostring(val))
+	end
+	table.insert(lines, "     .")
+
+	vim.api.nvim_set_option_value("modifiable", true, { buf = self.stack_buf })
+	vim.api.nvim_buf_set_lines(self.stack_buf, 0, -1, false, lines)
+	vim.api.nvim_set_option_value("modifiable", false, { buf = self.stack_buf })
+	if self.stack_win then
+		vim.api.nvim_win_set_cursor(self.stack_win, { #self.stack + 1, 0 })
+	end
+end
+
+function Calculator:init_stack_keymaps()
+	vim.keymap.set("n", "q", function()
+		for k, _ in ipairs(self.stack) do
+			self.stack[k] = nil
+		end
+		if self.stack_win then
+			vim.api.nvim_win_close(self.stack_win, true)
+		end
+		vim.api.nvim_buf_delete(self.stack_buf, {})
+	end, { buffer = self.stack_buf })
+	vim.keymap.set("n", "0", function()
+		self:insert_stack(0)
+	end, { buffer = self.stack_buf })
+	vim.keymap.set("n", "1", function()
+		self:insert_stack(1)
+	end, { buffer = self.stack_buf })
+	vim.keymap.set("n", "2", function()
+		self:insert_stack(2)
+	end, { buffer = self.stack_buf })
+	vim.keymap.set("n", "3", function()
+		self:insert_stack(3)
+	end, { buffer = self.stack_buf })
+	vim.keymap.set("n", "4", function()
+		self:insert_stack(4)
+	end, { buffer = self.stack_buf })
+	vim.keymap.set("n", "5", function()
+		self:insert_stack(5)
+	end, { buffer = self.stack_buf })
+	vim.keymap.set("n", "6", function()
+		self:insert_stack(6)
+	end, { buffer = self.stack_buf })
+	vim.keymap.set("n", "7", function()
+		self:insert_stack(7)
+	end, { buffer = self.stack_buf })
+	vim.keymap.set("n", "8", function()
+		self:insert_stack(8)
+	end, { buffer = self.stack_buf })
+	vim.keymap.set("n", "9", function()
+		self:insert_stack(9)
+	end, { buffer = self.stack_buf })
+end
+
+--------------------------------------------------------------------------------
+-------------------------------Stack Management---------------------------------
+--------------------------------------------------------------------------------
+
+---@param item number The number to add to the stack
+---@param index number? The position to insert it into (defaults to end)
+---@param redraw boolean? Redraw the stack buffer
+function Calculator:insert_stack(item, index, redraw)
+	if redraw == nil then
+		redraw = true
+	end
+	table.insert(self.stack, index or #self.stack, item)
+	if redraw then
+		self:draw_stack()
+	end
+end
+
+---@param index number? The position to remove from (defaults to end)
+---@param redraw boolean? Redraw the stack buffer
+---@return number item The removed item
+function Calculator:remove_stack(index, redraw)
+	if redraw == nil then
+		redraw = true
+	end
+	local item = table.remove(self.stack, index or #self.stack)
+	if redraw then
+		self:draw_stack()
+	end
+	return item
+end
+
+--------------------------------------------------------------------------------
+--------------------------------Module Exports----------------------------------
+--------------------------------------------------------------------------------
+
+function M.open()
+	local calculator = Calculator.create()
+	calculator:create_ui()
 end
 
 return M
